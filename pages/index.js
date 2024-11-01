9bf46455399703192b2a7f37844d3b90e743b47b
xandycosta <costecnologia2023@gmail.com>
2024-11-01 00:08:33 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+    return <h1>Nada supera o resultado! 
+        Guarde essas palavras minha princesa!
+        Ainda jantaremos na torre de Paris.
+    </h1>
+}
+
+export default Home;
+

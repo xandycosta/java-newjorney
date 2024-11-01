21fe740b5d63a1a520d3fb6233ba1c01c2d05d28
xandycosta <costecnologia2023@gmail.com>
2024-11-01 00:08:33 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+    return <h1>Deus já nos mostrou o caminho! 
+        cabe à nós agora, seguirmos em frente!
+        Tudo é possível para aquele que crê!
+    </h1>
+}
+
+export default Home;
+
